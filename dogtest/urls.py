d902d3bdbc4879532634eaf48535001983541684
gyounghee <ghl0209@naver.com>
2022-04-05 12:36:56 +0900

    urls.py name 지정

--- dogtest/urls.py
+++ dogtest/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dogobesitytest/', include('dogtest_app.urls'), name='mainpage'),
+    path('dogobesitytest/', include('dogtest_app.urls')),
 ] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- dogtest_app/urls.py
+++ dogtest_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [    
     ### login page가 따로 있지 않고, 초기 페이지에서 login이 가능하도록 하신거 같아서 url설정 따로 안함
-    path('', views.login),
+    path('', views.login, name='mainpage'),
     path('signup/', views.signup, name='signup'),
     path('dogimage/', views.imageupload, name='dogimage'),
     path('testresult/',views.testresult, name='testresult'),
