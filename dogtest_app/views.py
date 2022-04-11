60e33f9a98d786c3a354617c4f33ad400cad40e4
gyounghee <ghl0209@naver.com>
2022-04-12 00:11:41 +0900

    유효성 검증 코드 추가

--- dogtest_app/views.py
+++ dogtest_app/views.py
@@ -116,7 +116,8 @@ def imageupload( request ):
             ## 가장 최근 테스트의 비만율, 현재 테스트의 비만율과 text
             response_data = {'pre_rate' : recent_data.obesity_rate, 'cur_rate':testresult['obesity_rate'], 'cur_result':testresult['text']} 
             result_serializer = ResultSerializer(data =response_data)
-            return JsonResponse(result_serializer.data, status=201)
+            if result_serializer.is_valid():
+                return JsonResponse(result_serializer.data, status=201)
         else : 
             return HttpResponse( status= 400 )  
 
