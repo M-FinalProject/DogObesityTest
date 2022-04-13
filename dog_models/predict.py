1ed5e119caf56818e63c7da3e5ed6dc98faf0632
gyounghee <ghl0209@naver.com>
2022-04-13 16:59:11 +0900

    keras모델 함수 수정

--- dog_models/predict.py
+++ dog_models/predict.py
@@ -36,10 +36,10 @@ def img_predict_keras(dog_breed, selected_model, decode_img, img_name):
 
     predict_prob = model.predict(test)
 
-    accuracy = int(round(predict_prob[0][0],2) * 100) 
+    accuracy = int( 100 - ( round(predict_prob[0][0],2) * 100 ) ) 
     # print( round(predict_prob[0][0],2) * 100 )
 
-    if round(predict_prob[0][0],2) < 0.5 :
+    if round(predict_prob[0][0],2) > 0.5 :
         result = {'result':'정상', 'accuracy' : accuracy, 'text': "당신의 강아지는 정상입니다" }
         return result
     else :
